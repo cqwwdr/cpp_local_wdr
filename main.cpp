03ee86c5d4eb33f66d58807138fbcc1b65604d42
wangdongrong <527423058@qq.com>
2023-02-08 14:27:56 +0800

    main 文件

---
+++ main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by 王东荣 on 2023/2/8.
+//
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    for(int i=0;i<10;i++){
+        printf("%d\n",i++);
+    }
+    return 0;
+}
